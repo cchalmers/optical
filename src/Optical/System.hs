2fe7f5a6deff6f45317ff8da9092fb65ba45733e
Christopher Chalmers <c.chalmers@me.com>
2015-09-01 14:41:01 +0100

    Add System module

---
+++ src/Optical/System.hs
@@ -0,0 +1,28 @@
+module Optical.System
+  (
+    module System.FilePath
+  , module System.FilePath.Lens
+  , module System.Directory
+  , module System.Environment
+  , module System.Process
+
+  , mkdir
+  , ls
+  ) where
+
+import System.FilePath
+import System.FilePath.Lens
+import System.Directory
+import System.Environment
+import System.Process
+import Control.Monad.IO.Class
+
+
+mkdir :: MonadIO m => FilePath -> m ()
+mkdir = liftIO . createDirectoryIfMissing False
+
+ls :: MonadIO m => FilePath -> m [FilePath]
+ls dir = do
+  dir' <- liftIO $ canonicalizePath dir
+  ps <- liftIO $ drop 2 <$> getDirectoryContents dir'
+  return $ map (dir' </>) ps
