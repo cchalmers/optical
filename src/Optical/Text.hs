6a613b216807ba46da690d816754b0b028eeead6
Christopher Chalmers <c.chalmers@me.com>
2015-09-08 23:04:12 +0100

    Add lots of isos for Textual

--- src/Optical/Text.hs
+++ src/Optical/Text.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE TypeSynonymInstances  #-}
 
 module Optical.Text where
@@ -10,10 +11,17 @@ import           Optical.Containers
 import           Control.Lens
 
 import qualified Data.ByteString      as BS
+import qualified Data.ByteString.Char8 as BS8
+import qualified Data.ByteString.Lazy.Char8 as LBS8
 import qualified Data.ByteString.Lazy as LBS
 import qualified Data.List            as List
 import qualified Data.Text            as T
+import qualified Data.Text.Encoding            as TE
 import qualified Data.Text.Lazy       as LT
+import qualified Data.Text.Lazy.Encoding   as LTE
+
+import qualified Data.ByteString.Lazy.UTF8 as LUTF8
+import qualified Data.ByteString.UTF8 as UTF8
 
 import Prelude hiding (words, unwords, lines, unlines)
 
@@ -35,6 +43,7 @@ instance Chunked LBS.ByteString BS.ByteString where
 class Textual t where
   -- | Indexed traversal over individual characters of something 'Textual'
   chars :: IndexedTraversal' Int t Char
+  chars = string . traversed
 
   -- | Break a string up into a list of strings at newline characters.
   --   The resulting strings do not contain newlines.
@@ -64,17 +73,41 @@ class Textual t where
   worded f = fmap unwords . traversed f . words
   {-# INLINE worded #-}
 
+  -- | Isomorphism between a textual and text.
+  text :: Iso' t T.Text
+
+  -- | Isomorphism between a textual and lazy text.
+  lazyText :: Iso' t LT.Text
+
+  -- | Isomorphism between a textual and utf8 encoded bytestring.
+  utf8 :: Iso' t BS.ByteString
+
+  -- | Isomorphism between a textual and utf8 encoded lazy bytestring.
+  lazyUtf8 :: Iso' t LBS.ByteString
+
+  -- | Isomorphism between a textual and string.
+  string :: Iso' t String
+
 instance Textual T.Text where
   {-# INLINE chars #-}
   {-# INLINE words #-}
   {-# INLINE unwords #-}
   {-# INLINE lines #-}
   {-# INLINE unlines #-}
-  chars = _Text . chars
+  {-# INLINE text #-}
+  {-# INLINE lazyText #-}
+  {-# INLINE string #-}
+  {-# INLINE utf8 #-}
+  {-# INLINE lazyUtf8 #-}
   words = T.words
   lines = T.lines
   unwords = T.unwords
   unlines = T.unlines
+  text = id
+  lazyText = lazy
+  string = _Text
+  utf8 = iso TE.encodeUtf8 TE.decodeUtf8
+  lazyUtf8 = text . lazyUtf8
 
 instance Textual LT.Text where
   {-# INLINE chars #-}
@@ -82,28 +115,95 @@ instance Textual LT.Text where
   {-# INLINE unwords #-}
   {-# INLINE lines #-}
   {-# INLINE unlines #-}
-  chars = _LText . chars
+  {-# INLINE text #-}
+  {-# INLINE lazyText #-}
+  {-# INLINE string #-}
+  {-# INLINE utf8 #-}
+  {-# INLINE lazyUtf8 #-}
   words = LT.words
   lines = LT.lines
   unwords = LT.unwords
   unlines = LT.unlines
+  text = strict
+  lazyText = id
+  string = _LText
+  utf8 = text . utf8
+  lazyUtf8 = iso LTE.encodeUtf8 LTE.decodeUtf8
 
-instance Textual String where
+instance a ~ Char => Textual [a] where
   {-# INLINE chars #-}
   {-# INLINE words #-}
   {-# INLINE unwords #-}
   {-# INLINE lines #-}
   {-# INLINE unlines #-}
+  {-# INLINE text #-}
+  {-# INLINE lazyText #-}
+  {-# INLINE string #-}
+  {-# INLINE utf8 #-}
+  {-# INLINE lazyUtf8 #-}
   chars = traversed
   words = List.words
   lines = List.lines
   unwords = List.unwords
   unlines = List.unlines
+  text = from _Text
+  lazyText = from _LText
+  string = id
+  utf8 = iso UTF8.fromString UTF8.toString
+  lazyUtf8 = iso LUTF8.fromString LUTF8.toString
 
--- utf8String :: Iso' ByteString String
+-- | utf8 encoded.
+instance Textual BS.ByteString where
+  {-# INLINE chars #-}
+  {-# INLINE words #-}
+  {-# INLINE unwords #-}
+  {-# INLINE lines #-}
+  {-# INLINE unlines #-}
+  {-# INLINE text #-}
+  {-# INLINE lazyText #-}
+  {-# INLINE string #-}
+  {-# INLINE utf8 #-}
+  {-# INLINE lazyUtf8 #-}
+  words = BS8.words
+  lines = UTF8.lines
+  unwords = BS8.unwords
+  unlines = BS8.unlines
+  text = from utf8
+  lazyText = from utf8
+  string = from utf8
+  utf8 = id
+  lazyUtf8 = lazy
 
--- utf8 :: Iso' Text ByteString
--- utf8 = iso encodeUtf8 decodeUtf8
+-- | utf8 encoded.
+instance Textual LBS.ByteString where
+  {-# INLINE chars #-}
+  {-# INLINE words #-}
+  {-# INLINE unwords #-}
+  {-# INLINE lines #-}
+  {-# INLINE unlines #-}
+  {-# INLINE text #-}
+  {-# INLINE lazyText #-}
+  {-# INLINE string #-}
+  {-# INLINE utf8 #-}
+  {-# INLINE lazyUtf8 #-}
+  words = LBS8.words
+  lines = LUTF8.lines
+  unwords = LBS8.unwords
+  unlines = LBS8.unlines
+  text = from lazyUtf8
+  lazyText = from lazyUtf8
+  string = from lazyUtf8
+  utf8 = strict
+  lazyUtf8 = id
+
+-- utf8String :: Iso' String ByteString
+-- utf8String = iso LUTF8.fromString LUTF8.toString
+
+-- utf8 :: Iso' LT.Text ByteString
+-- utf8 = iso LTE.encodeUtf8 LTE.decodeUtf8
+
+-- lutf8 :: Iso' LT.Text ByteString
+-- lutf8 = iso LTE.encodeUtf8 LTE.decodeUtf8
 
 -- instance Building (Endo [a]) [a] where
 --   building =
