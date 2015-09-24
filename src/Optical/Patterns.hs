1a5172065b6549cc0cad75b39e203cd0a5e9e2b4
Christopher Chalmers <c.chalmers@me.com>
2015-09-24 15:55:14 +0100

    Remove cons/snoc patterns
    They're in lens now.

--- src/Optical/Patterns.hs
+++ src/Optical/Patterns.hs
@@ -11,19 +11,12 @@ module Optical.Patterns
   , pattern P4
 
 #if __GLASGOW_HASKELL__ >= 709
-    -- * Cons / Snoc
-  , pattern (:<)
-  , pattern (:>)
-  , pattern Empty
-
     -- Numbers
   , pattern NaN
   , pattern Infinity
 #endif
   ) where
 
-import Control.Lens
-import Control.Lens.Extras
 import Linear.Affine
 import Linear
 
@@ -33,24 +26,6 @@ pattern P2 x y = P (V2 x y)
 pattern P3 x y z = P (V3 x y z)
 pattern P4 x y z w = P (V4 x y z w)
 #if __GLASGOW_HASKELL__ >= 709
-
--- | Pattern for consing / unconsing items to a list like structure.
-pattern (:<) :: () => Cons s s a a => a -> s -> s
-pattern x :< xs <- (uncons -> Just (x, xs)) where
-  x :< xs = cons x xs
-
-infixr 5 :<
-
--- | Pattern for snocing / unsnocing items to a list like structure.
-pattern (:>) :: () => Snoc s s a a => s -> a -> s
-pattern xs :> x <- (unsnoc -> Just (xs, x)) where
-  xs :> x = snoc xs x
-
-infixl 5 :>
-
-pattern Empty :: () => AsEmpty t => t
-pattern Empty <- (is _Empty -> True) where
-  Empty = _Empty # ()
 
 pattern NaN :: () => RealFloat a => a
 pattern NaN <- (isNaN -> True) where
